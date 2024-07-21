ceb5852d79e9dfcc9a5a981f471c42ea9792b6c4
Atharv Vani <atharvavani110@gmail.com>
2024-07-21 21:40:44 +0530

    uuid middleware code updated

--- server/middlewares/uuid.middleware.js
+++ server/middlewares/uuid.middleware.js
@@ -4,10 +4,11 @@ export const uuidMiddleware = (req, res, next) => {
   let userUuid = req.cookies.uuid;
   if (!userUuid) {
     userUuid = uuidv4();
-    console.log("uuid not found");
     res.cookie("uuid", userUuid, {
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000, // Expires in 1 day
+      sameSite: "None", // Required for cross-origin cookies
+      secure: true, // Required if using SameSite=None
     });
   }
   req.userUuid = userUuid;
